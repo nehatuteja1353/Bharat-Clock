00093f0154b66395218001dfa31241a1a77e13d5
nehatuteja1353 <nehatuteja1353@gmail.com>
2023-11-22 15:47:49 -0600

    files added

---
+++ bharat clock/src/App.jsx
@@ -0,0 +1,17 @@
+import ClockHeading from "./components/ClockHeading";
+import ClockSlogan from "./components/ClockSlogan";
+import CurrentTime from "./components/CurrentTime";
+
+
+
+function App() {
+ return<div> 
+    <ClockHeading/>
+    <ClockSlogan/>
+    <CurrentTime/>
+    
+  
+   </div>
+}
+
+export default App;
---
+++ bharat clock/src/components/ClockHeading.jsx
@@ -0,0 +1,6 @@
+
+const ClockHeading = ()=>{
+return <h1 className="fw-bolder"> Bharat Clock</h1>
+};
+
+export default ClockHeading;
